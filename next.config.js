9fa3c7f2d278ad13eb6edae9c2171b50bea80386
Juno <cuongnguyen@fetch.tech>
2021-10-04 18:25:26 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,14 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
-
-module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack", "url-loader"],
-    });
-    return config;
-  },
 };
